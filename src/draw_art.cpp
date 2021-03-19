813d10ed4d2b0602030c39e2d642a00f4462989a
Keil Reedich <keilreedich@Keils-MacBook-Air.local>
2021-03-19 00:00:27 -0400

    final

--- src/draw_art.cpp
+++ src/draw_art.cpp
@@ -6,6 +6,31 @@ using namespace agl;
 int main(int argc, char** argv)
 {
    canvas drawer(640, 380);
-   // your code here
-}
+   drawer.background(0,0,0);
+   drawer.begin(POINTS);
+   drawer.color(255,255,255);
+   for(int i = 0; i<200; i++){
+      drawer.vertex(rand()%640, rand()%380);
+   }
+   drawer.end();
+   
+   for(int j = 0; j<rand()%2+30; j++){
+      if(rand()%2 == 0){
+         drawer.outline(true);
+      }
+      else{
+         drawer.outline(false);
+      }
+      drawer.color(rand()%256, rand()%256, rand()%256);
+      drawer.rectangle(rand()%(640-100)+50, rand()%(380-40)+20, rand()%50+10, rand()%20+20);
+   }
+   drawer.save("../rectangles_in_space.png");
 
+   ppm_pixel dif;
+   dif.r = rand()%256;
+   dif.g = rand()%256;
+   dif.b = 255;
+
+   drawer.differenceBlend(dif);
+   drawer.save("../rectangles_difference_blend.png");
+}
